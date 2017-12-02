6f1923951eed4e7790b956a07fbb7ec9eb392b32
Jose Lourenco <jose@lourenco.ws>
2017-12-02 18:13:33 +0000

    Add content of sir uploads

--- init/init.sh
+++ init/init.sh
@@ -3,8 +3,8 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "DROP DATABASE ${MYSQL_DATABASE};"
-mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE ${MYSQL_DATABASE};"
+#mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "DROP DATABASE ${MYSQL_DATABASE};"
+#mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE ${MYSQL_DATABASE};"
 
 if [ -n "$SENDY_BASE_DATA_FILE" ]; then
 #    mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < $SENDY_BASE_DATA_FILE
